d6dafd11adc7959e09418a4723c47414080af294
BrijeshSajeev <brijesh00300@gmail.com>
2023-05-26 22:56:15 +0530

    🚀🚀🚀

---
+++ EXERCISE/Test/dot.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+def draw_dot():
+    diameter = 50  # Diameter of the dot
+    x = 50  # x-coordinate of the dot's center
+    y = 50  # y-coordinate of the dot's center
+    canvas.create_oval(x, y, x + diameter, y + diameter, fill="red")
+
+
+root = tk.Tk()
+root.title("Red Dot Example")
+
+canvas = tk.Canvas(root, width=200, height=200)
+canvas.pack()
+
+button = tk.Button(root, text="Draw Dot", command=draw_dot)
+button.pack()
+
+root.mainloop()
